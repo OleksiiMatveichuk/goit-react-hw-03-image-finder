844399bdd0cd901c14c552bbd4954cd3ddc24333
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-05-31 23:33:33 +0300

    start proeject

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Searchbar } from './Searchbar';
+import { ImageGallery } from './ImageGallery';
+import { ImageGalleryItem } from './ImageGalleryItem';
+import { Loader } from './Loader';
+import { Button } from './Button';
+import { Modal } from './Modal';
+
+export class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+    );
+  }
+}
